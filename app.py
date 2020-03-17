398b6fdf5041503676047cee339c0f0ad782706c
Nihad Zeitouny <nihad.zeitouny@code.berlin>
2020-03-17 18:57:35 +0100

    started using dynamic names, showing a list of rooms in the home page

--- app.py
+++ app.py
@@ -1,14 +1,32 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+rooms = [
+
+    {
+        "name" : "HTML",
+        "number" : "3.10.7",
+        "capacity" : "10 People",
+        "equipment" : "TV + ChromeCast",
+    },
+
+    {
+        "name" : "Swift",
+        "number" : "3.10.8",
+        "capacity" : "15 People",
+        "equipment" : "TV",
+    },
+
+]
+
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", rooms=rooms)
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", title="About")
 
 if __name__ == '__main__':
     app.run(debug=True)
