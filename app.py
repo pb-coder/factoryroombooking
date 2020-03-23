fa9b9abb13992ed3e85d5d4c3964a6eba4c00e3c
Nihad Zeitouny <nihad.zeitouny@code.berlin>
2020-03-23 19:43:57 +0100

    Register/login forms (still buggy)

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
+app.config["SECRET_KEY"] = "84a67df87b0aa0bd95caaa4747538413"
 rooms = [
 
     {
@@ -27,6 +29,25 @@ def home():
 @app.route("/about")
 def about():
     return render_template("about.html", title="About")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}!", "csstag")
+        return redirect(url_for("home"))
+    return render_template("register.html", title="Register", form=form)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "test@test.com" and form.password.data == "password":
+            flash("You have been logged in!", "csstag")
+            return redirect(url_for("home"))
+        else:
+            flash("Login Unsuccessful. Please check username and password", "csstag")
+    return render_template("login.html", title="Login", form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
